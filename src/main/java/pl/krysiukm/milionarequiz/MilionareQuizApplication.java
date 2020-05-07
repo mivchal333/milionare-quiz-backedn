ec998bda9dca60157614e25619dff1607ad9f26e
mivchal <mivchal@gmail.com>
2020-05-07 22:43:51 +0200

    Initial project commit

---
+++ src/main/java/pl/krysiukm/milionarequiz/MilionareQuizApplication.java
@@ -0,0 +1,13 @@
+package pl.krysiukm.milionarequiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilionareQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilionareQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/krysiukm/milionarequiz/MilionareQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krysiukm.milionarequiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilionareQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

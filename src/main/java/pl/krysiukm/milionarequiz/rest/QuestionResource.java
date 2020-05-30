b54d7f5bc5faf758f71794d31a64231dcf094264
mivchal <mivchal@gmail.com>
2020-05-30 23:39:57 +0200

    resource cross changes

--- src/main/java/pl/krysiukm/milionarequiz/rest/QuestionResource.java
+++ src/main/java/pl/krysiukm/milionarequiz/rest/QuestionResource.java
@@ -3,10 +3,7 @@ package pl.krysiukm.milionarequiz.rest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.krysiukm.milionarequiz.bean.QuestionBean;
 import pl.krysiukm.milionarequiz.model.Difficulty;
 import pl.krysiukm.milionarequiz.model.Question;
@@ -27,7 +24,8 @@ public class QuestionResource {
     private final QuestionResourceValidator validator;
     private final BeanHelper beanHelper;
 
-    @RequestMapping("/difficulty")
+    @CrossOrigin
+    @RequestMapping
     ResponseEntity<List<QuestionBean>> getQuestions(
             @RequestParam(defaultValue = "NORMAL", name = "difficulty") String difficultyParam,
             @RequestParam(defaultValue = "20") int limit) {
